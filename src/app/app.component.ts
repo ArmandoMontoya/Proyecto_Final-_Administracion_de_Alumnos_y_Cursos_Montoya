bf199f51020b3b890ae1c0cc651dfc090071bcde
Ismael Díaz <ismael.diaz@codelabmx.com>
2022-07-28 19:39:10 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Proyecto_Final-_Administracin_de_Alumnos_y_Cursos_Montoya';
+}
